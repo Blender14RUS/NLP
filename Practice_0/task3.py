80408ca31193fff2bb63f9dda890f09b4e3424c0
Igor Ukrainets <bender14rus@gmail.com>
2018-11-14 14:22:35 +0300

    add task3 and task4

---
+++ Practice_0/task3.py
@@ -0,0 +1,22 @@
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+pathToFile = "./task2_text.txt"
+pathToResultFile = "./task3.txt"
+text = ""
+with open(pathToFile, 'r', encoding="utf-8") as file:
+    text = file.read()
+
+tokens = nltk.tokenize.regexp_tokenize(text, r'(\w+[-\w+]*)')
+
+#leaved only word
+#words = [word for word in tokens if word.isalpha()]
+
+#remove stoowords
+stop_words = set(stopwords.words('russian'))
+result = [word for word in tokens if word not in stop_words]
+
+with open(pathToResultFile, "w", encoding='utf-8') as output_file:
+    output_file.write('\n'.join(result))
+
