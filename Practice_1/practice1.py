c7daaed89662a818753186e51d9c6945a0946d0b
Igor <bender14rus@gmail.com>
2019-01-18 02:20:42 +0300

    updated

--- Practice_1/practice1.py
+++ Practice_1/practice1.py
@@ -1,41 +1,22 @@
-# # -*- coding: utf-8 -*-
-# import nltk
-# from nltk import Nonterminal, nonterminals, Production, CFG
-#
-#
-# grammar_rules = u"""
-# S -> NP VP
-# PP -> P NP
-# NP -> Det N N | Det N | 'цехе'
-# VP -> V VP | V PP
-# Det -> 'Эти'
-# N ->  'типы' | 'стали'
-# V -> 'стали' | 'есть'
-# P -> 'в'
-# """
-#
-# grammar = nltk.CFG.fromstring(grammar_rules)
-#
-# # Эти типы стали есть в цехе
-# sentence = u'Эти типы стали есть в цехе'.split()
-# parser = nltk.ChartParser(grammar)
-# for tree in parser.parse(sentence):
-#     print(tree)
 import nltk
 from nltk import Nonterminal, nonterminals, Production, CFG
 
-groucho_grammar = nltk.CFG.fromstring("""
+
+grammar_rules = u"""
 S -> NP VP
 PP -> P NP
-NP -> Det N | Det N PP | 'I'
-VP -> V NP | VP PP
-Det -> 'an' | 'my'
-N -> 'elephant' | 'pajamas'
-V -> 'shot'
-P -> 'in'
-""")
+NP -> Det N N | Det N | 'библиотеке'
+VP -> V VP | V PP
+Det -> 'Эти'
+N ->  'студенты' | 'начали'
+V -> 'начали' | 'петь'
+P -> 'в'
+"""
 
-sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
-parser = nltk.ChartParser(groucho_grammar)
-for tree in parser.parse(sent):
+grammar = nltk.CFG.fromstring(grammar_rules)
+
+# Эти типы стали есть в цехе
+sentence = u'Эти студенты начали петь в библиотеке'.split()
+parser = nltk.ChartParser(grammar)
+for tree in parser.parse(sentence):
     print(tree)
