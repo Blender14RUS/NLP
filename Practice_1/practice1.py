7c32f29b503ac9411a2a4c59f18d929311442de3
Igor Ukrainets <bender14rus@gmail.com>
2019-01-18 15:02:14 +0300

    update

--- Practice_1/practice1.py
+++ Practice_1/practice1.py
@@ -1,6 +1,4 @@
 import nltk
-from nltk import Nonterminal, nonterminals, Production, CFG
-
 
 grammar_rules = u"""
 S -> NP VP
@@ -9,14 +7,13 @@ NP -> Det N N | Det N | 'библиотеке'
 VP -> V VP | V PP
 Det -> 'Эти'
 N ->  'студенты' | 'начали'
-V -> 'начали' | 'петь'
+V -> 'начали' | 'шуметь'
 P -> 'в'
 """
 
 grammar = nltk.CFG.fromstring(grammar_rules)
 
-# Эти типы стали есть в цехе
-sentence = u'Эти студенты начали петь в библиотеке'.split()
+sentence = u'Эти студенты начали шуметь в библиотеке'.split()
 parser = nltk.ChartParser(grammar)
 for tree in parser.parse(sentence):
     print(tree)
