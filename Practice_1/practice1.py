9b914d208c098f88c3fdd8f197bf9355dafbe26f
Igor <bender14rus@gmail.com>
2019-01-20 02:40:19 +0300

    updated

--- Practice_1/practice1.py
+++ Practice_1/practice1.py
@@ -5,19 +5,20 @@ from nltk import Nonterminal, nonterminals, Production, CFG
 
 grammar_rules = u"""
 S -> NP VP
-PP -> P NP
-NP -> Det N N | Det N | 'цехе'
-VP -> V VP | V PP
+PP -> P N
+NP -> Det N N | Det N
+VP -> V VP | V PP | A VP
 Det -> 'Эти'
-N ->  'типы' | 'стали'
+N ->  'типы' | 'стали' | 'цехе'
 V -> 'стали' | 'есть'
 P -> 'в'
+A -> 'снова'
 """
 
 grammar = nltk.CFG.fromstring(grammar_rules)
 
 # Эти типы стали есть в цехе
-sentence = u'Эти типы стали есть в цехе'.split()
+sentence = u'Эти типы снова стали есть в цехе'.split()
 parser = nltk.ChartParser(grammar)
 for tree in parser.parse(sentence):
     print(tree)
--- Practice_6/train_model.py
+++ Practice_6/train_model.py
@@ -1,6 +1,26 @@
 from gensim.models.word2vec import LineSentence
 from gensim.models import FastText
+import nltk
+import numpy as np
+from nltk.collocations import *
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
 
+DEFAULT_PATH = 'data.txt'
+DEFAULT_ENCODING = 'utf-8'
+
+def read_file(path, encoding):
+    with open(path, 'r', encoding=encoding) as file:
+        lines = file.read()
+    return lines
+
+def tokenize(source_file):
+    tokens = nltk.word_tokenize(source_file)
+    tokens = [w.lower() for w in tokens]
+    print('----------------------------------------------------------------------------------------')
+    print(tokens[:10])
+    print('----------------------------------------------------------------------------------------\n')
+    return tokens
 
 def create_model(filename):
     sentences = LineSentence(filename)
@@ -9,11 +29,40 @@ def create_model(filename):
 
     return model
 
+def bigram(source_file):
+    trigram_measures = nltk.collocations.BigramAssocMeasures()
+    tokens = tokenize(source_file)
+    text = nltk.Text(tokens)
+    finder = BigramCollocationFinder.from_words(text)
+    bigrams = finder.nbest(trigram_measures.pmi, 100)
+    return bigrams
+
+def bagofwords(sentence, words):
+    # frequency word count
+    bag = np.zeros(len(words))
+    for sw in sentence:
+        for i, word in enumerate(words):
+            if word == sw:
+                bag[i] += 1
+
+    return np.array(bag)
 
 def main():
+    source_file = read_file(DEFAULT_PATH, DEFAULT_ENCODING)
     model = create_model("data.txt")
-    model.wv.save_word2vec_format("ft_model.vec")
 
+    bigrams = bigram(source_file)
+    wordlist = []
+    for word in bigrams:
+        wordlist.append(word[0])
+        wordlist.append(word[1])
+
+    vectors = []
+    for line in source_file.splitlines():
+        vectors.append(bagofwords(line, wordlist))
+    # model.wv.save_word2vec_format("ft_model.vec")
+
+    pickle.dump(vectors, open('model', 'wb'))
 
 if __name__ == "__main__":
     main()
